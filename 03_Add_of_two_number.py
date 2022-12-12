415d52af9f4f0753d5af026abb4ea46346329dba
Tejas Falke <tejasdf.77@gmail.com>
2022-12-12 23:59:39 +0530

    Addition of two numbers using function

---
+++ 03_Add_of_two_number.py
@@ -0,0 +1,8 @@
+def Add_of_two_number():
+    user_val1 = eval(input("First value : "))
+    user_val2 = eval(input("Second value : "))
+
+    sum_of_values = user_val1 + user_val2
+    print(f"Sum of {user_val1} and {user_val2} is = {sum_of_values}")
+
+Add_of_two_number()
